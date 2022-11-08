46cd616c1f6a27c672991aace7312563dba962dc
Benjamin D <deanb@marshall.edu>
2022-11-08 17:12:14 -0500

    Udpates

--- database/mysql_db.sql
+++ database/mysql_db.sql
@@ -4,6 +4,17 @@
 /*																				*/
 /*	MySQL Example DB													        */
 /*																				*/
-/*	We will begin here by adding and modify database code				        */
+/*	We will begin here by adding and modify database code this script is        */
+/*  is designed to be run over and over in a development environment. We will   */
+/*  be adding sample data as we go along. If you use this template once your    */
+/*  development environment becomes more stable you may want to restrict some   */
+/*  of the more destructive statements or create a second script for minor      */
+/*  database changes or revisions.                                              */
 /*																				*/
 /********************************************************************************/
+-- Drop the database
+drop database if exists cs410exdb;
+-- Create the database
+create database cs410exdb /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
+-- Let's use it and create some db objects
+USE cs410exdb;
