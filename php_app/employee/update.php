eeb59ac660e6eadb8a464bb68b18aeaef15f0a20
Benjamin Dean <deanb@marshall.edu>
2022-11-09 08:39:21 -0500

    Fix config.php reference

--- php_app/employee/update.php
+++ php_app/employee/update.php
@@ -8,7 +8,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;                                                                                                                                                              
 }    
 // Include config file
-require_once "config.php";
+require_once "../config.php";
  
 // Define variables and initialize with empty values
 $name = $address = $salary = "";
